6c53c3e6ae0d479f4999c0533cab5d3662b75394
RandomDrone <80712989+RandomDrone@users.noreply.github.com>
2023-01-04 16:53:05 +0100

    Squashed commit of the following:
    commit 55978393ba6e2b6ec316c03b022c522f659100b7
    Author: RandomDrone <80712989+RandomDrone@users.noreply.github.com>
    Date:   Wed Jan 4 16:49:03 2023 +0100
    
        changed title version text
    
        changed title version text
    
    commit 54c221ea2e60c1873ac009517e421e70085b45f7
    Author: RandomDrone <80712989+RandomDrone@users.noreply.github.com>
    Date:   Thu Dec 1 15:54:21 2022 +0100
    
        Update toDo.txt
    
    commit 63f2e8bd732aafdb56af76b1cccb0a31e16ebf47
    Author: RandomDrone <80712989+RandomDrone@users.noreply.github.com>
    Date:   Fri Nov 11 21:01:19 2022 +0100
    
        drobne_poprawki
    
    commit c0d63fa781b98c43c2eb3f910fe5ef752dc640b1
    Author: RandomDrone <80712989+RandomDrone@users.noreply.github.com>
    Date:   Mon Nov 7 09:26:50 2022 +0100
    
        auto detect seond key- wersja tymczasowa
    
    commit 544f06ea85c7910567ae84d9f21973bf749be43b
    Author: RandomDrone <80712989+RandomDrone@users.noreply.github.com>
    Date:   Sat Nov 5 22:31:01 2022 +0100
    
        backup
    
    commit 01d7fb60dd02cfe304c25da50d9f93e16699e9ae
    Author: RandomDrone <80712989+RandomDrone@users.noreply.github.com>
    Date:   Sat Nov 5 21:14:44 2022 +0100
    
        kosmetyka_i_sprzatenieKodu
    
    commit fc7098c0784bc334cf3503e0c4f35232136be799
    Author: RandomDrone <80712989+RandomDrone@users.noreply.github.com>
    Date:   Sat Nov 5 19:06:11 2022 +0100
    
        cos tam zaczelo dzialac

--- generatot.js
+++ generatot.js
@@ -1,15 +1,8 @@
+"use strict"
+
 function modifyChars(word, oldChar, newChar)
 {
     var newWord='';
-
-    // for(var i=0; i < word.length; i++)
-    // {
-    //     if (word[i] == oldChar)
-    //     {
-    //         newWord = newWord+newChar;  
-    //     }
-    //     else newWord = newWord+word[i];
-    // }
 
     for( let i of word)
     {
@@ -20,44 +13,100 @@ function modifyChars(word, oldChar, newChar)
         else newWord = newWord+i;
     }
 
-    console.log("modifyChars done")
     return newWord;
 }
-
-function incrementString()
-{
-    var newWord='';
-
-    for(var i=startVal; i<=endVal; i=i+step)
-    {
-        newWord = newWord+ modifyChars(sourceWord, key, i) + "<br>";
-    }
-
-    console.log("incrementString done")
-    return newWord;
-}
-
-    
-    var startVal;
-    var endVal;
-    var step;
-    var key;
-    var sourceWord;
-
 
 //handle start button
 document.getElementById("btnStart").onclick = function ()
 {
+    var result1 = [];
+    var result2 = [];
+    var finalResults =[];
+    var finalOutput = "";
+    var keyObj = 
+    {
+        startVal: 0,
+        endVal: 0,
+        step: 0,
+        key: ""
+    };
+    var keysArray = [
+        {
+            startVal: 0,
+            endVal: 0,
+            step: 0,
+            key: ""
+        }
+    ];
+
     //read input fields
-    startVal = parseInt(document.getElementById("valStart").value);
-    endVal = parseInt(document.getElementById("valEnd").value);
-    step = parseInt(document.getElementById("step").value);
-    key  = document.getElementById("key").value;
-    sourceWord = document.getElementById("input").value;
+    result1[0] = document.getElementById("input").value;
+
+    keysArray[0].startVal = parseInt(document.getElementById("valStart").value);
+    keysArray[0].endVal = parseInt(document.getElementById("valEnd").value);
+    keysArray[0].step = parseInt(document.getElementById("step").value);
+    keysArray[0].key  = document.getElementById("key").value;
 
     
-    document.getElementById("output").innerHTML=incrementString();
+    keyObj.startVal = parseInt(document.getElementById("valStart2").value);
+    keyObj.endVal = parseInt(document.getElementById("valEnd2").value);
+    keyObj.step = parseInt(document.getElementById("step2").value);
+    keyObj.key  = document.getElementById("key2").value;
+    //
+   if( !isNaN(keyObj.startVal) && !isNaN(keyObj.endVal) && !isNaN(keyObj.step) && keyObj.step > 0 && keyObj.key != "" )
+   {
+    console.log("obj added")
+    keysArray.push(keyObj);
+   }
+    
+    //incrementString
+    for(let k = 0; k < keysArray.length; k++)
+    {
+       if( (k%2) === 0 )
+       {
+            //result 1 -> result 2
+            result2.length = 0; //clear output
+            for(let word of result1)
+            {
+                for(let i = keysArray[k].startVal; i<=keysArray[k].endVal; i=i+keysArray[k].step)
+                {
+                    result2.push(modifyChars(word, keysArray[k].key, i));
+                }
+            }
+       }
+       else
+       {
+            //result 2 -> result 1
+            result1.length = 0; //clear output
+            for(let word of result2)
+            {
+                for(let i = keysArray[k].startVal; i<=keysArray[k].endVal; i=i+keysArray[k].step)
+                {
+                    result1.push(modifyChars(word, keysArray[k].key, i));
+                }
+            }
+       }
+    }
+    
+    if( (keysArray.length%2)===0 )
+    {
+       // console.log(result1);
+        finalResults = result1;
+    }
+    else
+    {
+        //console.log(result2);
+        finalResults = result2;
+    }
+    
+    for(let e of finalResults)
+    {
+        finalOutput = `${finalOutput}${e}<br>`
+    }
+   
+    document.getElementById("output").innerHTML = finalOutput;
+
+    //console.log("incrementString done")
+    
     console.log("main function done");
 };
-
-//console.log(incrementString(-1, 5, ".", "Jeden.Dwa."));
